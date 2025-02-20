a8355de3af11d3568dc0101043292d5fec63192e
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-20 14:46:50 +0000

    add exercise 2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,10 +4,14 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// A função main é o começo  para a execução do programa
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Bye world");
+            Console.WriteLine("Bye world!");
         }
     }
 }
