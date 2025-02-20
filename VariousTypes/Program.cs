d990f0104b26fc8d99ac9685f18390d43e1c8177
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-20 18:35:53 +0000

    add variables with integers

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,7 +6,15 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int x = 40;
+            uint j = 6U;
+            long l = 60L;
+            int hex = 0x05;
+            ulong n = 4000UL;
+            int bin = 0b101110;
+
+            Console.WriteLine($"x: {x}, j: {j}, l: {l}, hex: {hex}, n: {n}, bin : {bin} ");
+
         }
     }
 }
