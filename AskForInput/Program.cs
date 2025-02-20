6fac25ab13091297cd7d57b4abb0a32082dcde68
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-20 22:24:39 +0000

    add exercise 7

---
+++ AskForInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere um número inteiro: ");
+            string str = Console.ReadLine();
+            int i = int.Parse(str);
+
+            Console.WriteLine("Insere um número real: ");
+            string str2 = Console.ReadLine();
+            float f = float.Parse(str2);
+
+            float soma = i + f;
+
+            Console.WriteLine($"A soma dos números é: {soma}");
+        }
+    }
+}
