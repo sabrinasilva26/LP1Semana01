6c49b4ba443b95729b4b27b38569d7847510d971
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-20 14:54:13 +0000

    Add VariousTypes

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
