db4f2643e4be966e4654684b77aa08fe517e12c8
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-02-21 17:58:35 +0000

    add Smiley

---
+++ Smiley/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Smiley
+{
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Insere código: ");
+            string str = Console.ReadLine();
+           
+            int i = Convert.ToInt32(str);
+           
+            Console.WriteLine($"Caráter Unicode : {value} ");
+
+           
+
+
+
+
+            
+
+           
+        }
+    }
+}
